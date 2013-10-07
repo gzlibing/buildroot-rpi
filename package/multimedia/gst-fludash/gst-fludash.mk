f105ae2dc8812c090458f4043edf06b3f17a4d74
albertd <albert@dahan.nl>
2013-10-07 13:28:48 +0200

    rpi: adding codec pack and update dash plugin

--- package/multimedia/gst-fludash/gst-fludash.mk
+++ package/multimedia/gst-fludash/gst-fludash.mk
@@ -9,19 +9,14 @@ GST_FLUDASH_SITE_METHOD = local
 GST_FLUDASH_LICENSE = PROPRIETARY
 GST_FLUDASH_REDISTRIBUTE = NO
 
-GST_FLUDASH_DEPENDENCIES += libcurl
+GST_FLUDASH_DEPENDENCIES += gst-flucodecpack
 
 define GST_FLUDASH_INSTALL_TARGET_CMDS
 	if [ ! -f $(@D)/gstfludash.bin ]; then \
 		cat $(@D)/README; \
 	else \
 		mkdir -p $(TARGET_DIR)/usr/lib/gstreamer-0.10; \
-		$(INSTALL) -m 755 $(@D)/fludownloader.bin $(TARGET_DIR)/usr/lib/libfludownloader.so.0.0.0; \
 		$(INSTALL) -m 755 $(@D)/gstfludash.bin $(TARGET_DIR)/usr/lib/gstreamer-0.10/libgstfludash.so; \
-		(cd $(TARGET_DIR)/usr/lib/; \
-			ln -sfn libfludownloader.so.0.0.0 libfludownloader.so.0; \
-			ln -sfn libfludownloader.so.0 libfludownloader.so; \
-		) \
 	fi
 endef
 
