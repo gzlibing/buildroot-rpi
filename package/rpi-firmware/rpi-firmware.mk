fcb541c9c555a85918c7ed678fa72d89ff052c0f
albertd <albert@dahan.nl>
2013-10-12 12:26:06 +0200

    rpi-firmware: bump version

--- package/rpi-firmware/rpi-firmware.mk
+++ package/rpi-firmware/rpi-firmware.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-RPI_FIRMWARE_VERSION = e6b21d6a62c9a29d4e2dece0452908449b434400
+RPI_FIRMWARE_VERSION = 2a8fd62e523f8e77933456cfe87c66c029c719bd
 RPI_FIRMWARE_SITE = http://github.com/raspberrypi/firmware/tarball/master
 RPI_FIRMWARE_LICENSE = BSD-3c
 RPI_FIRMWARE_LICENSE_FILES = boot/LICENCE.broadcom
