ad2bdf696caf3b23e37867852cf40f98815287db
albertd <albert@dahan.nl>
2013-11-15 15:01:00 +0100

    qt5wayland: adding qwindow-compositor

--- package/qt5/qt5wayland/qt5wayland.mk
+++ package/qt5/qt5wayland/qt5wayland.mk
@@ -16,22 +16,26 @@ endef
 
 define QT5WAYLAND_BUILD_CMDS
  	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)
+ 	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)/examples/qwindow-compositor
 endef
 
 define QT5WAYLAND_INSTALL_STAGING_CMDS
 	$(MAKE) -C $(@D) install
+	$(MAKE) -C $(@D)/examples/qwindow-compositor install
 endef
 
 define QT5WAYLAND_INSTALL_TARGET_CMDS
 	cp -dpf $(STAGING_DIR)/usr/lib/libQt5Compositor*.so* $(TARGET_DIR)/usr/lib
 	cp -dpf $(STAGING_DIR)/usr/lib/qt/plugins/platforms/libqwayland-brcm-egl.so $(TARGET_DIR)/usr/lib/qt/plugins/platforms/
 	cp -dpfr $(STAGING_DIR)/usr/lib/qt/plugins/waylandcompositors $(TARGET_DIR)/usr/lib/qt/plugins/
+	cp -dpf $(STAGING_DIR)/usr/examples/qtwayland/qwindow-compositor/qwindow-compositor $(TARGET_DIR)/usr/bin
 endef
 
 define QT5WAYLAND_UNINSTALL_TARGET_CMDS
 	-rm $(TARGET_DIR)/usr/lib/libQt5Compositor*.so*
 	-rm $(TARGET_DIR)/usr/lib/qt/plugins/platforms/libqwayland-brcm-egl.so
 	-rm -r $(TARGET_DIR)/usr/lib/qt/plugins/waylandcompositors
+	-rm $(TARGET_DIR)/usr/bin/qwindow-compositor
 endef
 
 $(eval $(generic-package))
