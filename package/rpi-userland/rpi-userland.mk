9d31c8c2c1620696f4fd55bc365d3829d101d7ff
albertd <albert@dahan.nl>
2013-12-26 17:31:53 +0100

    rpi: bump version of kernel, firmware and userland

--- package/rpi-userland/rpi-userland.mk
+++ package/rpi-userland/rpi-userland.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-RPI_USERLAND_VERSION = d2c9f912da389c23ed27e81e940be3540f64fc59
+RPI_USERLAND_VERSION = aee3ec867b363b4b2349e38e6559f7aaa5bd6331
 RPI_USERLAND_SITE = http://github.com/raspberrypi/userland/tarball/master
 RPI_USERLAND_LICENSE = BSD-3c
 RPI_USERLAND_LICENSE_FILES = LICENCE
