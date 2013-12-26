b6a636d4d331bdadb8ce638b88a39838cb446318
albertd <albert@dahan.nl>
2013-12-26 23:12:55 +0100

    weston: bump version

--- package/weston/weston.mk
+++ package/weston/weston.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WESTON_VERSION = 1.3.1
+WESTON_VERSION = 1.3.91
 WESTON_SITE = http://wayland.freedesktop.org/releases/
 WESTON_SOURCE = weston-$(WESTON_VERSION).tar.xz
 WESTON_LICENSE = MIT
